f485a6b6468776f03740ccd4d18df96dc2684167
Georgios <farmaker47@gmail.com>
2024-06-03 17:18:56 +0300

    Clear ChatViewModel.kt, keep some implementation in comments

--- app/src/main/java/com/chatbot/ChatViewModel.kt
+++ app/src/main/java/com/chatbot/ChatViewModel.kt
@@ -1,20 +1,14 @@
 package com.chatbot
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.CreationExtras
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.launch
 
-class ChatViewModel(
-    private val inferenceModel: InferenceModel
-) : ViewModel() {
+class ChatViewModel : ViewModel() {
 
     // `GeminiUiState()` is optimized for Gemini.
     // Replace `GeminiUiState` with `ChatUiState()` if you're using a different model
@@ -30,11 +24,11 @@ class ChatViewModel(
     fun sendMessage(userMessage: String) {
         viewModelScope.launch(Dispatchers.IO) {
             _uiState.value.addMessage(userMessage, USER_PREFIX)
-            var currentMessageId: String? = _uiState.value.createLoadingMessage()
+            // var currentMessageId: String? = _uiState.value.createLoadingMessage()
             setInputEnabled(false)
             try {
                 val fullPrompt = _uiState.value.fullPrompt
-                inferenceModel.generateResponseAsync(fullPrompt)
+                /*inferenceModel.generateResponseAsync(fullPrompt)
                 inferenceModel.partialResults
                     .collectIndexed { index, (partialResult, done) ->
                         currentMessageId?.let {
@@ -49,7 +43,7 @@ class ChatViewModel(
                                 setInputEnabled(true)
                             }
                         }
-                    }
+                    }*/
             } catch (e: Exception) {
                 _uiState.value.addMessage(e.localizedMessage ?: "Unknown Error", MODEL_PREFIX)
                 setInputEnabled(true)
@@ -62,11 +56,6 @@ class ChatViewModel(
     }
 
     companion object {
-        fun getFactory(context: Context) = object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-                val inferenceModel = InferenceModel.getInstance(context)
-                return ChatViewModel(inferenceModel) as T
-            }
-        }
+
     }
 }
