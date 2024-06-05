99964f456597dba1aa8c9232c5d5cff9d3f85968
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-06-05 09:58:40 +0530

    replace TextField with mic/record Icon in ChatScreen.kt

--- app/src/main/java/com/chatbot/ChatScreen.kt
+++ app/src/main/java/com/chatbot/ChatScreen.kt
@@ -1,28 +1,26 @@
 package com.chatbot
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,8 +28,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -77,47 +75,19 @@ fun ChatScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 16.dp, horizontal = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
+            horizontalArrangement = Arrangement.Center
         ) {
-
-            Column { }
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            TextField(
-                value = userMessage,
-                onValueChange = { userMessage = it },
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.Sentences,
-                ),
-                label = {
-                    Text(stringResource(R.string.chat_label))
-                },
+            Icon(
+                Icons.Default.Mic,
+                contentDescription = stringResource(R.string.action_microphone),
                 modifier = Modifier
-                    .weight(0.85f),
-                enabled = textInputEnabled
-            )
-
-            IconButton(
-                onClick = {
-                    if (userMessage.isNotBlank()) {
-                        onSendMessage(userMessage)
-                        userMessage = ""
+                    .background(MaterialTheme.colorScheme.primaryContainer, CircleShape)
+                    .clip(CircleShape)
+                    .clickable {
+                        // TODO: Handle mic-button click
                     }
-                },
-                modifier = Modifier
-                    .padding(start = 16.dp)
-                    .align(Alignment.CenterVertically)
-                    .fillMaxWidth()
-                    .weight(0.15f),
-                enabled = textInputEnabled
-            ) {
-                Icon(
-                    Icons.AutoMirrored.Default.Send,
-                    contentDescription = stringResource(R.string.action_send),
-                    modifier = Modifier
-                )
-            }
+                    .padding(16.dp)
+            )
         }
     }
 }
